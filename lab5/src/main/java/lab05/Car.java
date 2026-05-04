49d7e72b00650fdbc522716d214c6be7da358892
dinithirajapaksha2003-create <dinithirajapaksha2003@gmail.com>
2026-05-04 22:13:04 +0530

    q2 lab05 car class

---
+++ lab5/src/main/java/lab05/Car.java
@@ -0,0 +1,19 @@
+
+package lab05;
+
+public class Car extends vehicle {
+    private int numberOfDays;
+    private double dailyRate;
+
+    public Car(String vehicleNumber, String brand, int numberOfDays, double dailyRate) {
+        super(vehicleNumber, brand);
+        this.numberOfDays = numberOfDays;
+        this.dailyRate = dailyRate;
+    }
+
+    @Override
+    public double calculateRentalCost() {
+        super.calculateRentalCost();
+        return numberOfDays * dailyRate;
+    }
+}

838ceeb1a33e1beaea1bffaceff1ffc1193884db
dinithirajapaksha2003-create <dinithirajapaksha2003@gmail.com>
2026-05-04 22:14:02 +0530

    q2 lab05 Bike class

---
+++ lab5/src/main/java/lab05/Bike.java
@@ -0,0 +1,19 @@
+
+package lab05;
+
+
+public class Bike extends vehicle {
+    private int numberOfHours;
+    private double hourlyRate;
+    
+    public Bike(String vehicleNumber, String brand, int numberOfHours, double hourlyRate) {
+        super(vehicleNumber,brand);
+        this.numberOfHours=numberOfHours;
+        this.hourlyRate=hourlyRate;
+    }
+    @Override
+    public double calculateRentalCost(){
+        super.calculateRentalCost();
+        return numberOfHours*hourlyRate;
+    }
+}
